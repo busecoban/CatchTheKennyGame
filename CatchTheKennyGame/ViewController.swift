ed0df7e4a96c40f307d434646cbad764ff6a7285
Buse Çoban <73944611+busecoban@users.noreply.github.com>
2024-09-02 12:08:04 +0300

    initial

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Buse Çoban on 2.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

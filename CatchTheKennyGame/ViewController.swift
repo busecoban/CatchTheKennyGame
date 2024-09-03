cfcaf4dd53b08960759fd4532418fdd7d84b06df
Buse Çoban <73944611+busecoban@users.noreply.github.com>
2024-09-03 09:15:33 +0300

    thiefs are running,we have timer,we can replay

--- CatchTheKennyGame/ViewController.swift
+++ CatchTheKennyGame/ViewController.swift
@@ -11,6 +11,14 @@ class ViewController: UIViewController {
     
     var score = 0
     
+    var timer = Timer()
+    
+    var counter = 0
+    
+    var hideTimer = Timer()
+    
+    var thiefArray = [UIImageView]()
+    
     @IBOutlet weak var timeLabel: UILabel!
     
     @IBOutlet weak var scoreLabel: UILabel!
@@ -34,6 +42,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var thief8: UIImageView!
     
     @IBOutlet weak var thief9: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,12 +82,69 @@ class ViewController: UIViewController {
         thief9.addGestureRecognizer(recognizer9)
         
         
+        thiefArray = [ thief1 , thief2, thief3, thief4, thief5, thief6, thief7, thief8, thief9]
+        
+        counter = 10
+        timeLabel.text = String(counter)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(decreaseTime), userInfo: nil, repeats: true)
+        
+        hideTimer = Timer.scheduledTimer(timeInterval: 0.8, target: self, selector: #selector(hideThief), userInfo: nil, repeats: true)
+        
+        hideThief()
+        
+    }
+    
+    @objc func hideThief(){
+        for thief in thiefArray{
+            thief.isHidden=true
+        }
+        
+       let random = Int ( arc4random_uniform(UInt32(thiefArray.count-1)))
+        
+        thiefArray[random].isHidden=false
+        
         
     }
     
     @objc func increaseScore(){
         score += 1
         scoreLabel.text="Score: \(score)"
+
+    }
+    @objc func decreaseTime(){
+        counter -= 1
+        timeLabel.text=String(counter)
+        
+        if(counter==0){
+            timer.invalidate()
+            hideTimer.invalidate()
+            
+            for thief in thiefArray{
+                thief.isHidden=true
+            }
+            
+            let alert = UIAlertController(title: "The end", message: "Do you want to play again?", preferredStyle: UIAlertController.Style.alert)
+            
+            let okButton = UIAlertAction(title:"ok" ,style: UIAlertAction.Style.cancel ,handler: nil)
+            
+            let replayButton = UIAlertAction(title:"Replay" ,style: UIAlertAction.Style.default){
+                (UIAlertAction) in
+                
+                self.score = 0
+                self.scoreLabel.text="Score: \(self.score)"
+                self.counter = 10
+                self.timeLabel.text=String(self.counter)
+                
+                self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.decreaseTime), userInfo: nil, repeats: true)
+                
+                self.hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.hideThief), userInfo: nil, repeats: true)
+            }
+            
+            alert.addAction(okButton)
+            alert.addAction(replayButton)
+            self.present(alert,animated: true,completion: nil)
+            
+        }
 
     }
 
